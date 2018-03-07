a65ceb70966987b09ae72cf19c53e5f69bd4ef7b
Jayme Moehlee <jem614@gmail.com>
2018-03-06 19:18:20 -0600

    Note section added, and additional styling.

--- dont_forget_py/wsgi.py
+++ dont_forget_py/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+#import sys
 
 from django.core.wsgi import get_wsgi_application
 
-
+#sys.path.insert(0, '/opt/python/current/app')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dont_forget_py.settings")
 
 application = get_wsgi_application()
